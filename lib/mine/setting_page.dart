9b00b805047eec84682767bcb7a306bdf450b4b6
ningyuwen <aduning0331@gmail.com>
2019-06-02 17:38:37 +0800

    设置页显示版本号

--- lib/mine/setting_page.dart
+++ lib/mine/setting_page.dart
@@ -12,6 +12,7 @@ import 'package:my_mini_app/util/const_util.dart';
 import 'package:my_mini_app/util/snack_bar_util.dart';
 import 'package:my_mini_app/util/toast_util.dart';
 import 'package:my_mini_app/webview/webview_page.dart';
+import 'package:package_info/package_info.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -33,22 +34,42 @@ class SettingPage extends StatelessWidget {
   }
 
   Widget _bodyWidget(BuildContext context) {
-    return ListView.separated(
-        itemBuilder: (context, index) {
-          switch (index) {
-            case 0:
-              return _openSourceWidget(context);
-            case 1:
-              return _checkUpdateWidget();
-            case 2:
-              return _logoutWidget(context);
-          }
-        },
-        separatorBuilder: (context, index) =>
-            Divider(
-              height: 0.0,
+    return Stack(
+      children: <Widget>[
+        ListView.separated(
+            itemBuilder: (context, index) {
+              switch (index) {
+                case 0:
+                  return _openSourceWidget(context);
+                case 1:
+                  return _checkUpdateWidget();
+                case 2:
+                  return _logoutWidget(context);
+              }
+            },
+            separatorBuilder: (context, index) => Divider(
+                  height: 0.0,
+                ),
+            itemCount: 4),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 20.0),
+            child: FutureBuilder(
+              future: _getVersion(),
+              builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                if (snapshot.hasData) {
+                  print("version is: ${snapshot.data}");
+                  return Text("V${snapshot.data}");
+                } else {
+                  return CupertinoActivityIndicator();
+                }
+              },
             ),
-        itemCount: 4);
+          ),
+        )
+      ],
+    );
   }
 
   void _logout() async {
@@ -76,8 +97,7 @@ class SettingPage extends StatelessWidget {
         Navigator.push(
             context,
             MaterialPageRoute(
-                builder: (context) =>
-                new WebViewPage(
+                builder: (context) => new WebViewPage(
                     PAGE_NAME_OPEN_SOURCE, "https://github.com/ningyuwen")));
       },
     );
@@ -130,6 +150,11 @@ class SettingPage extends StatelessWidget {
       },
     );
   }
+
+  Future<String> _getVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    return packageInfo.version;
+  }
 }
 
 class _CheckUpdateWidget extends StatefulWidget {
@@ -172,14 +197,14 @@ class _CheckUpdateWidgetState extends State<_CheckUpdateWidget> {
               children: <Widget>[
                 _checkUpdateProvider.streamBuilder(
                     success: (CheckUpdateBeen checkUpdateBeen) {
-                      if (checkUpdateBeen.newVersion) {
-                        return Text("可升级最新版本${checkUpdateBeen.newVerName}",
-                            style: const TextStyle(fontSize: 14.0));
-                      } else {
-                        return Text("当前已是最新版本",
-                            style: const TextStyle(fontSize: 14.0));
-                      }
-                    }, loading: () {
+                  if (checkUpdateBeen.newVersion) {
+                    return Text("可升级最新版本${checkUpdateBeen.newVerName}",
+                        style: const TextStyle(fontSize: 14.0));
+                  } else {
+                    return Text("当前已是最新版本",
+                        style: const TextStyle(fontSize: 14.0));
+                  }
+                }, loading: () {
                   return CupertinoActivityIndicator(radius: 8.0);
                 }, error: (error) {
                   return Text("");
@@ -238,7 +263,7 @@ class _CheckUpdateWidgetState extends State<_CheckUpdateWidget> {
 
   void downloadApk(String downloadUrl) async {
     Directory downloadsDirectory =
-    await DownloadsPathProvider.downloadsDirectory;
+        await DownloadsPathProvider.downloadsDirectory;
     print(downloadsDirectory.path);
     final taskId = await FlutterDownloader.enqueue(
       url: downloadUrl,
@@ -246,12 +271,11 @@ class _CheckUpdateWidgetState extends State<_CheckUpdateWidget> {
       showNotification: true,
       // show download progress in status bar (for Android)
       openFileFromNotification:
-      true, // click on notification to open downloaded file (for Android)
+          true, // click on notification to open downloaded file (for Android)
     );
 
     print(
-        "完整的路径是：${downloadsDirectory.path +
-            downloadUrl.substring(downloadUrl.lastIndexOf("/"))}");
+        "完整的路径是：${downloadsDirectory.path + downloadUrl.substring(downloadUrl.lastIndexOf("/"))}");
 
     showDialog(
         context: context,
@@ -299,19 +323,19 @@ class _MyDialogState extends State<_MyDialog> {
   @override
   void initState() {
     FlutterDownloader.registerCallback(
-            (String id, DownloadTaskStatus status, int progress) {
-          // code to update your UI
-          print(progress);
-          if (id == widget._taskId) {
-            setState(() {
-              widget._progress = progress;
-            });
-          }
-          if (status == DownloadTaskStatus.complete) {
-            checkPermissionAndInstall(widget.filePath);
-            Navigator.pop(context);
-          }
+        (String id, DownloadTaskStatus status, int progress) {
+      // code to update your UI
+      print(progress);
+      if (id == widget._taskId) {
+        setState(() {
+          widget._progress = progress;
         });
+      }
+      if (status == DownloadTaskStatus.complete) {
+        checkPermissionAndInstall(widget.filePath);
+        Navigator.pop(context);
+      }
+    });
     super.initState();
   }
 
@@ -320,8 +344,8 @@ class _MyDialogState extends State<_MyDialog> {
         .checkPermissionStatus(PermissionGroup.storage);
     if (permission != PermissionStatus.granted) {
       Map<PermissionGroup, PermissionStatus> permissions =
-      await PermissionHandler()
-          .requestPermissions([PermissionGroup.storage]);
+          await PermissionHandler()
+              .requestPermissions([PermissionGroup.storage]);
       if (permissions[PermissionGroup.storage] == PermissionStatus.granted) {
         //安装应用
         return true;
@@ -357,38 +381,37 @@ class _MyDialogState extends State<_MyDialog> {
   Widget build(BuildContext context) {
     return Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new CircularPercentIndicator(
-              radius: 120.0,
-              lineWidth: 13.0,
-              animation: false,
-              percent: widget._progress * 1.0 / 100,
-              center: new Text(
-                "${widget._progress}%",
-                style: new TextStyle(
-                    fontWeight: FontWeight.bold, fontSize: 20.0),
-              ),
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        new CircularPercentIndicator(
+          radius: 120.0,
+          lineWidth: 13.0,
+          animation: false,
+          percent: widget._progress * 1.0 / 100,
+          center: new Text(
+            "${widget._progress}%",
+            style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+          ),
 //          footer: new Text(
 //            "Downloading...",
 //            style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 17.0),
 //          ),
-              circularStrokeCap: CircularStrokeCap.round,
-              progressColor: Colors.purple,
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            RaisedButton(
-              onPressed: () {
-                //取消
-                Navigator.pop(context);
-                FlutterDownloader.cancel(taskId: widget._taskId);
-              },
-              child: Text("取消"),
-            )
-          ],
-        ));
+          circularStrokeCap: CircularStrokeCap.round,
+          progressColor: Colors.purple,
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        RaisedButton(
+          onPressed: () {
+            //取消
+            Navigator.pop(context);
+            FlutterDownloader.cancel(taskId: widget._taskId);
+          },
+          child: Text("取消"),
+        )
+      ],
+    ));
   }
 
   void setProgress(int progress) {
